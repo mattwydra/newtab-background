8f1b21fc56e1e26bddceb1597c72625e49849df6
mattwydra <matthewwydra@gmail.com>
2025-02-10 16:08:15 -0500

    Create copy of current codebase to build upon

---
+++ v4/newtab.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const wallpapers = ["wallpapers/guts2.jpg", "wallpapers/guts.jpg"];
+    const predefinedSites = [
+        { url: "https://www.google.com", title: "Google" },
+        { url: "https://www.youtube.com", title: "YouTube" },
+        { url: "https://www.speedtest.net/", title: "Speed Test" },
+        { url: "https://www.github.com", title: "GitHub" },
+        { url: "https://mattwydra.github.io/projects/", title: "Minigames" },
+        { url: "https://mattwydra.github.io/about-me/", title: "About Me" },
+        { url: "https://mattwydra.github.io/LLM-integration/", title: "Media Recs" },
+        { url: "https://github.com/mattwydra/discord_bots", title: "Bots Repo" },
+        { url: "https://leetcode.com/", title: "LeetCode" },
+        { url: "https://neetcode.io/", title: "NeetCode" },
+        { url: "https://www.wikipedia.org", title: "Wikipedia" },
+        { url: "https://discord.com/", title: "Discord" },
+        { url: "https://mail.google.com/mail", title: "Mail" },
+        { url: "https://www.linkedin.com", title: "LinkedIn" },
+        { url: "https://codesandbox.io", title: "CodeSandbox" },
+        { url: "https://www.amazon.com", title: "Amazon" },
+
+    ];
+
+    function setRandomBackground() {
+        const randomImage = wallpapers[Math.floor(Math.random() * wallpapers.length)];
+        document.body.style.background = `url('${randomImage}') no-repeat center center fixed`;
+        document.body.style.backgroundSize = "cover";
+        // document.body.style.animation = "backgroundSlide 10s infinite ease-in-out";
+    }
+
+    setRandomBackground();
+    setInterval(setRandomBackground, 10000);
+
+    const topSitesContainer = document.getElementById("top-sites");
+
+    predefinedSites.forEach(site => {
+        const link = document.createElement("a");
+        link.href = site.url;
+        link.className = "top-site";
+        link.innerHTML = `<img src="https://www.google.com/s2/favicons?sz=64&domain=${site.url}" alt=""> <span>${site.title}</span>`;
+        topSitesContainer.appendChild(link);
+    });
+});
